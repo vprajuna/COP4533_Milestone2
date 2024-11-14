36061ea0772195c77b0e59afec584dfb8882ba3f
Alyssa Eriksen <aeriksen@ufl.edu>
2024-11-13 19:39:45 -0500

    program3 algorithm fixed

--- program3.cpp
+++ program3.cpp
@@ -3,75 +3,107 @@
 #include <tuple>
 #include <algorithm>
 #include <unordered_map>
+#include <string>
+#include <climits>
 using namespace std;
 
-// helper func for memoization key
-string make_key(int index, int current_width) {
-    return to_string(index) + "," + to_string(current_width);
+// helper func to make unique key for memoization
+string make_key(int index, int current_width, int current_height) {
+    return to_string(index) + "," + to_string(current_width) + "," + to_string(current_height);
 }
 
-// recursive function to  help calc the min cost with memoization
-int min_height_cost(int index, int current_width, int current_height, const vector<int>& heights,
-                    const vector<int>& widths, int W, std::unordered_map<string, int>& memo) {
+// recursive function to calculate the cost of min.
+int min_cost(int index, int current_width, int current_height, const vector<int>& heights,
+             const vector<int>& widths, int W, unordered_map<string, int>& memo,
+             unordered_map<string, bool>& start_new_platform) {
     if (index == heights.size()) {
-        return current_height; // if index is size of heights, all sculptures have been placed
+        // base case all have been placed
+        return current_height;
     }
 
-    string key = make_key(index, current_width);
+    // unique key to identify the current state of the platform
+    string key = make_key(index, current_width, current_height);
     if (memo.find(key) != memo.end()) {
-        return memo[key]; // if memo has been generated, then the computed cost exists for this config. so return stored res
+        // return cached if not avail
+        return memo[key];
     }
 
+    // add current sculpture to the current platform if width
     int cost_with_current = INT_MAX;
-    if (current_width + widths[index] <= W) { // if sculpture does not exceed W add to platform. update variables accordingly, call min_height_cost (recursively) to update for next increment index
-        cost_with_current = min_height_cost(index + 1, current_width + widths[index],
-                                            max(current_height, heights[index]),
-                                            heights, widths, W, memo);
+    if (current_width + widths[index] <= W) {
+        cost_with_current = min_cost(index + 1, current_width + widths[index],
+                                     max(current_height, heights[index]),
+                                     heights, widths, W, memo, start_new_platform);
     }
 
-    // starting a new platform with current sculpture
-    // adds current_height to existing platform, finalize, start new. reset current_width / current_height to height/w of sculp
-    // recursive call min_height_cost for next sculpture
-    int cost_with_new_platform = current_height + min_height_cost(index + 1, widths[index],
-                                                                  heights[index], heights, widths, W, memo);
+    // start a new platform with the current sculpture
+    int cost_with_new_platform = current_height + min_cost(index + 1, widths[index],
+                                                           heights[index], heights, widths, W, memo, start_new_platform);
 
-    // store minimum of two options and return (stored in memo)
-    memo[key] = min(cost_with_current, cost_with_new_platform);
+    // store the minimum of the two options in memo and return it
+    if (cost_with_new_platform < cost_with_current) {
+        memo[key] = cost_with_new_platform;
+        // new platfor mehre
+        start_new_platform[key] = true;
+    } else {
+        memo[key] = cost_with_current;
+        // new platform nto here
+        start_new_platform[key] = false;
+    }
+
     return memo[key];
-
 }
 
-tuple<int, int, vector<int>> program3(int n, int W, vector<int> heights, vector<int> widths) {
-    unordered_map<string, int> memo; // stores computed results
-    int min_cost = min_height_cost(0, 0, 0, heights, widths, W, memo); // set to 0
-    int num_platforms = 0;
-    vector<int> sculptures;
-    int current_width = 0, max_height = 0, num_sculptures = 0;
+// anmother. helper function to reconstruct platforms based on the memoized decisions
+vector<int> reconstruct_platforms(int n, int W, const vector<int>& heights, const vector<int>& widths,
+                                  const unordered_map<string, bool>& start_new_platform) {
+    vector<int> platforms;
+    int current_width = 0;
+    int num_sculptures = 0;
+    int current_height = 0;
 
-    // loop over each sculpture to decide if it fits on current platform. if adding and it exceeds W, start new platform
     for (int i = 0; i < n; ++i) {
-        if (current_width + widths[i] > W) {
-            sculptures.push_back(num_sculptures);
-            num_platforms++;
+        // use the same key construction to check memoized decisions
+        string key = make_key(i, current_width, current_height);
+
+        if (i > 0 && start_new_platform.at(key)) {
+            // a new platform starts here and then finalize the prev.
+            platforms.push_back(num_sculptures);
             current_width = 0;
-            max_height = 0;
             num_sculptures = 0;
+            current_height = 0;
         }
 
-        // add width of curr sculpture to curr width
-        // update max height to tallest platform
-        // max func. compares max_height with current sculpture and will swap if current is taller
+        // add the current sculpture to the current platform
         current_width += widths[i];
-        max_height = max(max_height, heights[i]);
+        current_height = max(current_height, heights[i]);
         num_sculptures++;
     }
-    // each time new platform starts, count is pushed to sculptures
-    sculptures.push_back(num_sculptures);
-    num_platforms++;
 
-    // returns tuple with nm platforms, min cost, and the count of sculptures on each platform
-    return make_tuple(num_platforms, min_cost, sculptures);
+    // add the last platform if there are any left
+    if (num_sculptures > 0) {
+        platforms.push_back(num_sculptures);
+    }
+
+    return platforms;
 }
+
+// main function to execute the solution. finally
+tuple<int, int, vector<int>> program3(int n, int W, const vector<int>& heights, const vector<int>& widths) {
+    unordered_map<string, int> memo;
+    unordered_map<string, bool> start_new_platform;
+
+    // compute the minimum cost using the recursive function
+    int min_cost_value = min_cost(0, 0, 0, heights, widths, W, memo, start_new_platform);
+
+    // reconstruct the platforms based on memoized decisions
+    vector<int> platforms = reconstruct_platforms(n, W, heights, widths, start_new_platform);
+
+    // return the tuple with the ## of platforms, min cost, and the sculpture count on each platform
+    return make_tuple(platforms.size(), min_cost_value, platforms);
+}
+
+
 
 int main(){
     int n, W;
@@ -92,3 +124,4 @@ int main(){
     }
     return 0;
 }
+
